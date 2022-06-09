3b00d39f79d68b099315cffa4fceb59c411d5f76
Samachariak <samuelkamau298@gmail.com>
2022-06-09 09:42:51 +0300

    first commit

---
+++ styles/app.js
@@ -0,0 +1,12 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns= document.querySelectorAll('.controls');
+const sectbtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+function pagetransition(){
+    for (let i = 0; i<sectbtn.length; i++){
+        sectbtn[i].addEventListener('click',()=>{
+            let currentbtn = document.querySelectorAll('.actve-btn')
+        })
+    }
+}
